f5d5c397f77347d707452a3284a513085e230422
Farhan <farhansyed11.3@gmail.com>
2023-04-05 00:43:20 -0700

    search readded

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,12 +1,22 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import useSWR from 'swr';
 
 import NavigationBar from '../components/NavigationBar';
 
-function Post() {
+const fetcher = (url) => axios.get(url).then((res) => res.data);
+function useFetcher() {
   const { id } = useParams();
-  const [post, setPost] = useState([]);
+  const { data, error, isLoading } = useSWR(`http://localhost:3000/api/posts/${id}`, fetcher);
+  return {
+    post: data,
+    isLoading,
+    isError: error,
+  };
+}
+
+function Post() {
+  const { post, isLoading } = useFetcher();
 
   const badges = () => {
     const string = post.options;
@@ -14,18 +24,7 @@ function Post() {
     return array.map((value) => <span key={value} className="badge mr-2">{value}</span>);
   };
 
-  useEffect(() => {
-    async function fetch() {
-      try {
-        const response = await axios.get(`http://localhost:3000/api/posts/${id}`);
-        setPost(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    fetch();
-  }, [id]);
+  if (isLoading) return <h1>loading</h1>;
 
   function formatMoney(num) {
     const amount = num / 100;
@@ -40,67 +39,70 @@ function Post() {
     return formatMoney(total);
   }
 
-  if (post && post.car) {
-    return (
-      <div className="container min-w-full">
-        <NavigationBar />
-        <div className="hero min-h-screen bg-base-200">
-          <div className="hero-content flex-col lg:flex-col">
-            <div className="w-full">
-              <p className="py-2">
+  const date = new Date(post.purchase_date);
+  return (
+    <div className="container min-w-full">
+      <NavigationBar />
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-col">
+          <div className="w-full">
+
+            <div className="flex justify-between">
+              <div>
                 <span className="badge badge-primary">{post.condition}</span>
-              </p>
-              <h1 className="text-5xl lg:text-6xl font-semibold">
-                {post.car.year}
-                {' '}
-                {post.car.make}
-                {' '}
-                {post.car.trim}
-              </h1>
-              <p className="py-1">
-                {post.car.transmission}
-              </p>
-              <p className="py-1">
-                {post.car.engine}
-              </p>
-              <p className="py-1">
-                {badges()}
-              </p>
-            </div>
-            <div>
-              <img src={post.image_url} className="object-scale-down rounded-lg shadow-2xl mb-4" alt="Purchase Info" />
-            </div>
-
-            <div className="stats stats-vertical xl:stats-horizontal bg-secondary text-secondary-content dark:text-primary-content w-full">
-              <div className="stat">
-                <div className="stat-title">MSRP</div>
-                <div className="stat-value">{formatMoney(post.msrp)}</div>
               </div>
-              <div className="stat">
-                <div className="stat-title">Tax</div>
-                <div className="stat-value">{formatMoney(post.tax)}</div>
-              </div>
-              <div className="stat">
-                <div className="stat-title">Market Adjustment</div>
-                <div className="stat-value">{formatMoney(post.market_adjustment)}</div>
-              </div>
-              <div className="stat">
-                <div className="stat-title">Other Fees</div>
-                <div className="stat-value">{formatMoney(post.fees)}</div>
-              </div>
-              <div className="stat bg-success text-primary dark:text-black">
-                <div className="stat-title">Total</div>
-                <div className="stat-value">{totalCost(post.msrp, post.market_adjustment, post.fees, post.tax)}</div>
+              <div className="order-last">
+                <p className="font-medium">{date.toLocaleDateString()}</p>
               </div>
             </div>
 
+            <h1 className="text-5xl lg:text-6xl font-semibold">
+              {post.car.year}
+              {' '}
+              {post.car.make}
+              {' '}
+              {post.car.trim}
+            </h1>
+            <p className="py-1">
+              {post.car.transmission}
+            </p>
+            <p className="py-1">
+              {post.car.engine}
+            </p>
+            <p className="py-1">
+              {badges()}
+            </p>
           </div>
+          <div>
+            <img src={post.image_url} className="object-scale-down rounded-lg shadow-2xl mb-4" alt="Purchase Info" />
+          </div>
+
+          <div className="stats stats-vertical xl:stats-horizontal bg-secondary text-secondary-content dark:text-primary-content w-full">
+            <div className="stat">
+              <div className="stat-title">MSRP</div>
+              <div className="stat-value">{formatMoney(post.msrp)}</div>
+            </div>
+            <div className="stat">
+              <div className="stat-title">Tax</div>
+              <div className="stat-value">{formatMoney(post.tax)}</div>
+            </div>
+            <div className="stat">
+              <div className="stat-title">Market Adjustment</div>
+              <div className="stat-value">{formatMoney(post.market_adjustment)}</div>
+            </div>
+            <div className="stat">
+              <div className="stat-title">Other Fees</div>
+              <div className="stat-value">{formatMoney(post.fees)}</div>
+            </div>
+            <div className="stat bg-success text-primary dark:text-black">
+              <div className="stat-title">Total</div>
+              <div className="stat-value">{totalCost(post.msrp, post.market_adjustment, post.fees, post.tax)}</div>
+            </div>
+          </div>
+
         </div>
       </div>
-    );
-  }
-  return (
-    <div>Loading...</div>
+    </div>
   );
 }
 
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,7 @@
-import algoliasearch from 'algoliasearch/lite';
-import {
-  InstantSearch, Hits, SearchBox, useInstantSearch,
-} from 'react-instantsearch-hooks-web';
-
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import NavigationBar from '../components/NavigationBar';
-
-const searchClient = algoliasearch('OBW05FG76H', '062d08f89cb6e6543fb21a15d161420d');
 
 function formatMoney(num) {
   const amount = num / 100;
@@ -16,93 +11,101 @@ function formatMoney(num) {
   }).format(amount);
 }
 
-function Hit({ hit }) {
-  const date = new Date(hit.purchase_date);
+function SearchInput(props) {
+  const [searchText, setSearchText] = useState('');
+  const handleClick = () => {
+    props.callback(searchText);
+  };
   return (
-    <div>
-      <div>
-        <div className="card-body">
-          <h2 className="card-title">
-            <div className="badge badge-primary">{hit.condition}</div>
-            {`${hit.car.year} ${hit.car.make} ${hit.car.trim}`}
-          </h2>
-          {date.toLocaleDateString()}
-          <p>{hit.options}</p>
-          <p>{formatMoney(hit.total_cost)}</p>
-          <div className="card-actions justify-end">
-            <Link
-              to={`/post/${hit.objectID}`}
-              className="btn btn-primary btn-sm"
+    <div className="grid grid-row-1 place-content-center py-10">
+      <div className="form-control">
+        <div className="input-group">
+          <input type="text" placeholder="Search 'BMW M550i in 92602'" className="input input-bordered border-2 w-96" onChange={(e) => { setSearchText(e.target.value); }} />
+          <button
+            type="button"
+            className="btn btn-square"
+            onClick={() => handleClick()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              View
-            </Link>
-          </div>
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
   );
 }
 
-const queryHook = (query, search) => {
-  search(query);
-};
-
-function SubmitIcon() {
+function CardItem({ item }) {
+  const date = new Date(item.purchase_date);
   return (
-    <div className="btn rounded-l-none w-14 btn-primary">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-      </svg>
+    <div className="card-body bg-base-100 shadow-xl">
+      <div className="flex justify-between">
+        <div>
+          <h2 className="card-title"><div className="badge badge-primary">{item.condition}</div></h2>
+        </div>
+        <div className="order-last">
+          <p className="font-light">{date.toLocaleDateString()}</p>
+        </div>
+      </div>
+      <h2 className="card-title">
+        {`${item.year} ${item.make} ${item.trim}`}
+      </h2>
+      <p>{item.options}</p>
+      <p>{formatMoney(item.total)}</p>
+      <div className="card-actions justify-end">
+        <Link
+          to={`/post/${item.id}`}
+          className="btn btn-primary btn-sm"
+        >
+          View
+        </Link>
+      </div>
     </div>
   );
 }
 
-function ResetIcon() {
+function Card({ list }) {
   return (
-    <></>
+    <div className="grid grid-cols-1 place-content-center sm:grid-cols-2 xl:grid-cols-4 gap-5 px-16 2xl:px-72 pb-14">
+      {list.map((item) => (
+        <CardItem item={item} key={item.id} />
+      ))}
+    </div>
   );
-}
-
-function EmptyQueryBoundary({ children, fallback }) {
-  const { indexUiState } = useInstantSearch();
-
-  if (!indexUiState.query) {
-    return fallback;
-  }
-
-  return children;
 }
 
 function Search() {
+  const [list, setList] = useState([]);
+
+  async function fetch(text) {
+    const reformatted = text.replace(/\s/g, '&');
+    try {
+      const response = await axios.get(`http://localhost:3000/api/search/${reformatted}`);
+      console.log(response.data)
+      setList(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const searchCallback = (text) => {
+    fetch(text);
+  };
+
   return (
     <div className="container min-w-full min-h-screen bg-base-200">
       <NavigationBar />
-      <InstantSearch searchClient={searchClient} indexName="post">
-        <SearchBox
-          placeholder="BMW M550i in 92602"
-          searchAsYouType={false}
-          autoFocus
-          submitIconComponent={SubmitIcon}
-          resetIconComponent={ResetIcon}
-          queryHook={queryHook}
-          classNames={{
-            root: 'grid grid-row-1 justify-center form-control my-10', form: 'input-group', input: 'input input-bordered border-2 w-96', submit: 'rounded-none', loadingIndicator: 'hidden',
-          }}
-        />
-        <EmptyQueryBoundary fallback={null}>
-          <Hits
-            classNames={{ list: 'grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-5 px-16 2xl:px-72 pb-14', item: 'card bg-base-100 shadow-xl' }}
-            hitComponent={Hit}
-          />
-        </EmptyQueryBoundary>
-      </InstantSearch>
+      <SearchInput callback={searchCallback} />
+      <Card list={list} />
     </div>
   );
 }
+
 export default Search;
