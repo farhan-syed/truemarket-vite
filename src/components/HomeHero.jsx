4cb5a9397c4e039bb0aef1e1a14402c063164ed4
Farhan <farhansyed11.3@gmail.com>
2023-04-25 23:08:52 -0700

    added button

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -7,18 +7,28 @@ function HomeHero() {
     navigate('/search');
   }
 
+  function post() {
+    navigate('/post/new');
+  }
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-2xl">
           <h1 className="text-7xl font-bold">Avoid Overpaying</h1>
-          <p className="py-6 text-2xl">
-            Make smart car buying decisions
+          <p className="my-6 text-3xl">
+            Make smarter car buying decisions
           </p>
-          <div className="form-control">
-            <div className="justify-center">
-              <button className="btn btn-success btn-md" type="button" onClick={search}>
+          <div className="flex flex-col gap-4">
+            <div className="flex-1">
+              <button className="btn btn-success btn-wide" type="button" onClick={search}>
                 Start your search
+              </button>
+            </div>
+            <div className="flex-1">
+              <p className="m-2">Purchased recently?</p>
+              <button className="btn bg-primary-content text-black hover:text-white btn-wide" type="button" onClick={post}>
+                Post your car
               </button>
             </div>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,10 @@ import HomeHero from '../components/HomeHero';
 
 function Home() {
   return (
-    <div className="container min-w-full">
-      <NavigationBar />
+    <div className="container w-screen relative min-w-full">
+      <div className="absolute w-screen">
+        <NavigationBar />
+      </div>
       <HomeHero />
     </div>
   );
